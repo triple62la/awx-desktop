d139f627aab9b0f388c0229d0f3ee33deb85ff87
rakhimzhon.dovidov <rkdovidov@rt-dc.ru>
2023-10-27 11:15:56 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+
+<template>
+  
+  <h1>AWX Desktop</h1>
+  <form >
+    <input type="text" name="esr-input" id="esr-input">put
+  </form>
+
+</template>
+
+
+
+<style scoped>
+
+</style>
